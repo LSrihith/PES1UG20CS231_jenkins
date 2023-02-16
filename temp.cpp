27bd73a9b50b11574d0113d60bf599a3f12e84f2
LSrihith <94776642+LSrihith@users.noreply.github.com>
2023-02-16 20:09:55 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World I am Lagisetty Srihith\n";
+  cout << "I am in Task5 my SRN is PES1UG20CS231\n";
+  return 0;
+}
